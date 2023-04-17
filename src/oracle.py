feae5cf6b22d6c0fcc86390efe6305ad5ff3c30e
ZRiley36 <zachriley36@gmail.com>
2023-04-16 23:17:34 -0700

    wrote submit() and verify(), still need to change cbc encrypt to return string

---
+++ src/oracle.py
@@ -0,0 +1,40 @@
+from Crypto import Random
+from Crypto.Cipher import AES
+import cbc
+import sys
+
+def urlEncode(data):
+    data = data.replace(';', '%3B')
+    data = data.replace('=', '%3D')
+    return data
+            
+        
+def submit(key, iv, data):
+    filename = "oraclestr.txt"
+    prepend = "userid-456;userdata="
+    appendstr = ";session-id-31337"
+    data = urlEncode(data)
+    concatstr = prepend+data+appendstr
+    f = open(filename, 'w')
+    f.write(concatstr)
+    f.close()
+    return cbc.encrypt(filename, key, iv) 
+# padding happens in cbc encrypt
+# need to refactor csc encrypt so it can return the cipher text
+    
+    
+    
+def verify(key, iv, ciphertext):
+    cipher = AES.new(key, AES.MODE_EBC, iv)
+    plaintext  = cipher.decrypt(ciphertext)
+    cipher.verify()
+
+def main():
+    key = Random.get_random_bytes(16)
+    iv = Random.get_random_bytes(16)
+    usrdata  = input('submit your message: ')
+    submit(key, iv, usrdata)
+
+
+if __name__ == "__main__":
+    main()
