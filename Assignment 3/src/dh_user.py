75eb94f3b2fb8fdcf548a594c103594b3dddf430
jogara2001 <ogarajoe1@gmail.com>
2023-04-24 12:26:16 -0700

    Implemented task 1 with low q and alpha

--- Assignment 3/src/dh_user.py
+++ Assignment 3/src/dh_user.py
@@ -1,8 +1,27 @@
-
+from Crypto.Cipher import AES
+from Crypto.Random import random
+from Crypto.Hash import SHA256
+from Crypto.Util import Padding
 
 class DHUser:
-    def __init__(self, q, alpha):
+    def __init__(self, q, alpha, iv):
+        self.secret_key = None
+        self.iv = iv
         self.q = q
         self.alpha = alpha
-        self.private_key = ""
-        self.public_key = ""
+        self.private_key = random.randint(0, q)
+        self.public_key = pow(alpha, self.private_key, q)
+
+    def compute_secret_key(self, other_public_key):
+        self.secret_key = SHA256.new(
+            pow(other_public_key, self.private_key, self.q).to_bytes(length=16)
+        ).hexdigest()[0:16]
+
+    def encrypt_message(self, message):
+        cbc = AES.new(bytes(self.secret_key, "utf-8"), AES.MODE_CBC, iv=self.iv)
+        padded = Padding.pad(bytes(message, "utf-8"), 16)
+        return cbc.encrypt(padded)
+
+    def decrypt_message(self, cypher_text):
+        cbc = AES.new(bytes(self.secret_key, "utf-8"), AES.MODE_CBC, iv=self.iv)
+        return Padding.unpad(cbc.decrypt(cypher_text), 16)
--- Assignment 3/src/diffie_hellman_simulation.py
+++ Assignment 3/src/diffie_hellman_simulation.py
@@ -1,10 +1,24 @@
 from Crypto import Random
+
 from dh_user import DHUser
+
+
 def simulate_dh():
-    alice = DHUser(q=37, alpha=5)
-    bob = DHUser(q=37, alpha=5)
+    iv = Random.get_random_bytes(16)
+    alice = DHUser(q=37, alpha=5, iv=iv)
+    bob = DHUser(q=37, alpha=5, iv=iv)
 
+    alice.compute_secret_key(bob.public_key)
+    bob.compute_secret_key(alice.public_key)
 
+    print("Bob's: ", bob.secret_key)
+    print("Alice's: ", alice.secret_key)
+
+    alice_cypher_text = alice.encrypt_message("Hi Bob!")
+    print("Bob received: ", bob.decrypt_message(alice_cypher_text))
+
+    bob_cypher_text = bob.encrypt_message("Hi Alice!")
+    print("Alice received: ", alice.decrypt_message(bob_cypher_text))
 
 
 if __name__ == "__main__":
